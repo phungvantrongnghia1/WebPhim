0d46bc95714625d32ed8f34a4997a5d872cd60f0
nghia <nghia@gmail.com>
2019-05-30 22:17:13 +0700

    Lam Lai

--- src/app/home/phong-ve/ghe/ghe.component.ts
+++ src/app/home/phong-ve/ghe/ghe.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-ghe',
@@ -7,12 +8,19 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class GheComponent implements OnInit {
 @Input() ghe;
+@Output() eventData = new EventEmitter();
   public status =false;
+  objectGhe={};
   constructor() { }
 
   ngOnInit() {
   }
   datGhe(){
     this.status =!this.status;
+    this.objectGhe={
+      trangThai:this.status,
+      ghe:this.ghe
+    }
+    this.eventData.emit(this.objectGhe);
   }
 }
--- src/app/home/phong-ve/phong-ve.component.ts
+++ src/app/home/phong-ve/phong-ve.component.ts
@@ -10,6 +10,8 @@ import { QuanLyPhimService } from 'src/_core/services/quan-ly-phim.service';
 export class PhongVeComponent implements OnInit {
   public maLichChieu;
   public chiTietPhong;
+  public danhSachGheDat=[];
+  public tien=0;
   constructor(private activatedRoute:ActivatedRoute,private QLPhim:QuanLyPhimService) { }
 
   ngOnInit() {
@@ -19,11 +21,35 @@ export class PhongVeComponent implements OnInit {
   LayChiTietPhongVe(){
     this.QLPhim.layChiTietPhongVe(this.maLichChieu).subscribe((data)=>{
       this.chiTietPhong = data;
-      console.log(data);
     });
   }
   layURL(){
     this.maLichChieu = this.activatedRoute.snapshot.paramMap.get('maChieu');
     console.log(this.maLichChieu);
+  }
+  datGhe(event){
+    console.log(event);
+    if(event.trangThai){
+      this.danhSachGheDat.push(event.ghe);
+     this.tien = this.tongTien();
+    }
+    else{
+      let index =  this.danhSachGheDat.findIndex((item)=>{
+        return item.TenGhe === event.ghe.TenGhe;
+      })
+      this.danhSachGheDat.splice(index,1);
+      console.log(event.ghe.GiaVe);
+      this.hieuTien(event.ghe.GiaVe);
+    }
+  }
+  tongTien(){
+    let sum =0;
+    this.danhSachGheDat.map((item)=>{
+      sum+=item.GiaVe;
+    })
+   return sum;
+  }
+  hieuTien(item){
+     this.tien-=item;
   }
 }
